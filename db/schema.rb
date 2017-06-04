fadea47675fa50c833e04eede24542c3ada5c0f9
Erik Thiem <erikthiem@gmail.com>
2017-06-04 14:09:33 -0400

    Added code to game schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170604152339) do
+ActiveRecord::Schema.define(version: 20170604180834) do
 
   create_table "creators", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20170604152339) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id"
+    t.string "code"
     t.index ["creator_id"], name: "index_games_on_creator_id"
   end
 
